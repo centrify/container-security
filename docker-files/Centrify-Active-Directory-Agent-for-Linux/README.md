6008d51ee78012e2995c2ccb9d821031eb08aff3
Ankit Chandra <41493745+ankitchandra18@users.noreply.github.com>
2019-12-19 14:09:48 -0800

    Squashed more bugs

--- docker-files/Centrify-Active-Directory-Agent-for-Linux/README.md
+++ docker-files/Centrify-Active-Directory-Agent-for-Linux/README.md
@@ -1,10 +1,11 @@
-# Enable CentrifyDC Agent for Linux in a CentOS container
+# Enable DirectControl and DirectAudit agents for Linux in a CentOS container
 
 ## Setup the docker image
 
 ### Build your own docker image
 You can just download the docker file [dockerfile.centos.adjoin](dockerfile.centos.adjoin) as a template and modify it for your own requirements.  You need to download the following files to your docker working directory:
-* [dockerfile.centos.adjoin](dockerfile.centos.adjoin): docker file
+* [dockerfile.centos.adjoin](dockerfile.centos.adjoin): docker file for enabling DirectControl agent only
+* [dockerfile.centos.adjoinaudit](dockerfile.centos.adjoinaudit): docker file for enabling DirectControl and DirectAudit agents
 * [centrify.repo](centrify.repo): information for Centrify repository
 * [adjoin_startup.sh](adjoin_startup.sh): start up file
 * [centrifydc-adleave.service](centrifydc-adleave.service): service definition for CentrifyDC service.  Used to leave the AD domain when the docker container is stopped.
@@ -62,6 +63,7 @@ Before using the docker file, please modify the **centrify.repo** file and repla
 ```
 baseurl=https://1234567890ABCDEFGHIJKLMNOPQRS%40centrify:1234567890abcdef1234567890abcdef12345678@repo.centrify.com/rpm-redhat/
 ```
+If you don't have a centrify repo then create an account in the Centrify Support website (http://www.centrify.com/support). Follow the instruction in https://www.centrify.com/support/customer-support-portal/repos to generate the repo key. Use the string before @repo.centrify.com to set up the parameter CENTRIFY_REPO_CREDENTIAL. For example, if the repo key is **1111111122222233333333333333344444555555:777888880000099999991234567890abcdefghij@repo.centrify.com**, set **CENTRIFY_REPO_CREDENIIAL=1111111122222233333333333333344444555555:777888880000099999991234567890abcdefghij**
 
 #### Build arguments for docker build command
 
@@ -81,11 +83,15 @@ Follow these steps to build the CentOS docker image:
 1. Download the above four files to the working directory.
 1. Modify the **centrify.repo** file and replace $CENTRIFY_REPOSITORY_KEY with your key.
 1. Prepare the **adjoiner.keytab** file, which contains the credential for adjoin when the container is being started.
-1. Modify the docker file if needed.
+1. Select the docker file to use (**dockerfile.centos.adjoin** or **dockerfile.centos.adjoinaudit**), modify the docker file if needed.
 1. Run **docker build** to build the image:
 ```
 docker build -t "centos:adjoin" \
     --build-arg DOMAIN=example.com --build-arg ADJOINER=zoneadm@EXAMPLE.COM -f ./dockerfile.centos.adjoin .
+```
+```
+docker build -t "centos:adjoinaudit" \
+    --build-arg DOMAIN=example.com --build-arg ADJOINER=zoneadm@EXAMPLE.COM -f ./dockerfile.centos.adjoinaudit .
 ```
 
 ## Environment variables for docker run command
@@ -100,7 +106,13 @@ You can customize your docker container by specifying environment variables usin
 | URL | The tenant URL | Yes* | example.my.centrify.com | If ENABLE_USE_MY_ACCOUNT is yes, this parameter must be specified. |
 | ADJOIN_OPTION | Optional parameters for adjoin command | Yes | -i | adjoin will not preload cache with -i option. |
 | COMPUTER_ROLES | Computer roles that are effective on the docker container, specified as a comma separate list. | Yes | docker_containers |
+| ENABLE_NSS_AUDITING | Enable or disable NSS auditing | Yes | no | This parameter only take effect if DirectAudit is installed in the docker image. By default, NSS auditing is enabled. You can disable NSS auditing with the value 'no' or 'false'.  |
+| INSTALLATION | Specify the audit installation | Yes | MyInstallation | This parameter only take effect if DirectAudit is installed in the docker image. If this parameter is not specified, "DefaultInstallation" will be used. Note that group policy can override this parameter. |
 | ROOT_PASSWORD | Initial root password. | Yes | | By default, this parameter should not be used and randomized password will be generated.   Set this parameter only for debugging purpose as it is easy for someone to monitor and access this information. |
+| CODE | Enrollment Code | Yes | *a long string* | | If enabling Use My Account feature then this parameter must be specified |
+| ADDRESS | IP address of the container host. | Yes | *10.11.1.1* | You must specify this if you want users to login using the Admin Portal.  This IP address must be reachable from the Centrify connector specified in the **CONNECTOR** parameter. If ENABLE_USE_MY_ACCOUNT is yes, this parameter must be specified. |
+| PORT | Port number used by SSH daemon | Yes | 2001 | Note: If you specify the ADDRESS parameter, you should specify a different value for this parameter since the docker container shares the same IP address and port as the container host. If ENABLE_USE_MY_ACCOUNT is yes, this parameter must be specified. |
+| CONNECTOR | The Centrify Connector to use when accessing this docker container using the Admin Portal. | YES | **connector1,connector2** | Default: any Centrify Connector will be used.  If you need to specify multiple connectors, separate them by commas. |
 
 \* The parameter is optional only if the default value has been specified in the build argument at build time.
 
@@ -112,10 +124,16 @@ docker run -d -p 2010:22 -v /sys/fs/cgroup:/sys/fs/cgroup --cap-add=SYS_ADMIN \
 ```
 docker run -d -p 2010:22 -v /sys/fs/cgroup:/sys/fs/cgroup --cap-add=SYS_ADMIN \
     --name container1 -e NAME=container1 \
-    -e ENABLE_USE_MY_ACCOUNT=yes -e URL=example.my.centrify.com \
+    -e ENABLE_USE_MY_ACCOUNT=yes -e URL=example.my.centrify.com -e CODE=12345678-ABCD-EFGH-IJKL-12345678ABCD\
+    -e ADDRESS=10.11.1.1 -e PORT=2001 -e CONNECTOR=connector1,connector2\
     centos:adjoin
 ```
-Note that you must specify **-v /sys/fs/cgroup:/sys/fs/cgroup --cap-add=SYS_ADMIN** in the **docker run** command.
+Note that you must specify **-d -v /sys/fs/cgroup:/sys/fs/cgroup --cap-add=SYS_ADMIN** in the **docker run** command.
+
+## Enable auditing with DirectAudit
+If the docker file **dockerfile.centos.adjoinaudit** is used to build the docker image, the DirectAudit agent will be started automatically when the container starts up. The NSS auditing is enabled by default, it can be disabled with the **ENABLE_NSS_AUDITING** parameter.
+
+Please see more auditing parameters in the above section.
 
 ## Setup docker to use multi-factor authentication (MFA)
 MFA requires the following setup:
@@ -124,7 +142,7 @@ MFA requires the following setup:
 
 ## Stopping the docker image
 
-The docker image always runs **adleave** itself when it is stopped.  However, the default timeout for the **docker stop** command may be too short to complete the operation.  We recommend to use a longer timeout such as 180 seconds.
+The docker image always runs **adleave** (and **cunenroll** when Use My Account feature is enabled) itself when it is stopped.  However, the default timeout for the **docker stop** command may be too short to complete the operation.  We recommend to use a longer timeout such as 180 seconds.
 
 An example of the **docker stop** command:
 ```
@@ -134,6 +152,6 @@ where *container1* is the name of the docker container.
 
 Note that the docker image automatically runs **adjoin** (based on the parameter settings in the original **docker run** command) when it is restarted.
 
-## Limitations
+## Notes
 
-* The adjoin post-join hook, **/usr/share/centrifydc/bin/ad_postjoin.sh**, will NOT be run after join.
+* The docker file is expected to work with the latest Centrify Infrastructure Service release. Problem may arise if a package repository other than the Centrify repository is used.
--- docker-files/Centrify-Active-Directory-Agent-for-Linux/adjoin_startup.sh
+++ docker-files/Centrify-Active-Directory-Agent-for-Linux/adjoin_startup.sh
@@ -6,38 +6,65 @@ echo "zone:" $ZONE
 echo "container:" $OU
 echo "host name to use:" $NAME
 
+# setup value for NSS auditing
+echo "NSS auditing:" $ENABLE_NSS_AUDITING
+NSSAuditing='Y'
+
+if [ "$ENABLE_NSS_AUDITING" != "" ] ; then
+   NSSAuditing=${ENABLE_NSS_AUDITING::1}
+fi
+
 # setup value for UseMyAccount
 echo "ENABLE_USE_MY_ACCOUNT: $ENABLE_USE_MY_ACCOUNT" 
 UseMyAccount='N'
 
 if [ "$ENABLE_USE_MY_ACCOUNT" != "" ] ; then
-   UseMyAccount=${ENABLE_USE_MY_ACCOUNT::1}
+    UseMyAccount=${ENABLE_USE_MY_ACCOUNT::1}
 fi
 echo "current tenant URL: " $URL
 echo "UseMyAccount: $UseMyAccount" 
+echo "Enrollment Code: " $CODE
+echo "IP address to use:" $ADDRESS
+echo "current setting for PORT:" $PORT
+echo "connectors setting: " $CONNECTOR
 
 # check required parameters
 
 if [ "$DOMAIN" = "" ]; then
-  echo "No DOMAIN specified."
-  exec /usr/sbin/init
+    echo "No DOMAIN specified."
+    exec /usr/sbin/init
 fi
 
 if [ "$ADJOINER" = "" ]; then
-  echo "No ADJOINER specified."
-  exec /usr/sbin/init
+    echo "No ADJOINER specified."
+    exec /usr/sbin/init
 fi
 
 if [ "$ZONE" = "" ]; then
-  echo "No ZONE specified."
-  exec /usr/sbin/init
+    echo "No ZONE specified."
+    exec /usr/sbin/init
 fi
 
 if [[ "$UseMyAccount" == "Y" || "$UseMyAccount" == "y" || "$UseMyAccount" == "T" || "$UseMyAccount" == "t" ]] ; then
-  if [ "$URL" = "" ]; then
-    echo "UseMyAccount is enabled but tenant URL is not specified."
-    exec /usr/sbin/init
-  fi
+    if [ "$URL" = "" ]; then
+        echo "UseMyAccount is enabled but tenant URL is not specified."
+        exec /usr/sbin/init
+    fi
+    if [ "$CODE" = "" ]; then
+        echo "UseMyAccount is enabled but Enrollment code is not specified."
+        exec /usr/sbin/init
+    fi
+    if [ "$ADDRESS" = "" ]; then
+        echo "UseMyAccount is enabled but IP Address of host is not specified."
+        exec /usr/sbin/init
+    fi
+    if [ "$PORT" = "" ]; then
+        echo "UseMyAccount is enabled but External Port of host is not specified."
+        exec /usr/sbin/init
+    fi
+    if [ "$CONNECTOR" = "" ]; then
+        echo "if UseMyAccount is enabled and an appropriate Connector is not specified, it may fail."
+    fi
 fi
 
 # touch all ignore files to set link count to 1
@@ -74,15 +101,15 @@ fi
 # setup for "Use My Account" feature
 
 if [[ "$UseMyAccount" == "Y" || "$UseMyAccount" == "y" || "$UseMyAccount" == "T" || "$UseMyAccount" == "t" ]] ; then
-  # set up sshd config
-  if [ ! -f /etc/ssh/sshd_config.bak ] ; then
-    cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-    sed -i -e '/^TrustedUserCAKeys/d' -e '/^AuthorizedPrincipalsCommand/d' /etc/ssh/sshd_config
-    echo "AuthorizedPrincipalsCommand /usr/bin/adquery user -P %u" >> /etc/ssh/sshd_config
-    echo "AuthorizedPrincipalsCommandUser root" >> /etc/ssh/sshd_config
-    echo "TrustedUserCAKeys /etc/ssh/cps_ca.pub" >> /etc/ssh/sshd_config
-  fi
-  /usr/share/centrifydc/bin/curl -o /etc/ssh/cps_ca.pub https://$URL/servermanage/getmastersshkey
+    # set up sshd config
+    if [ ! -f /etc/ssh/sshd_config.bak ] ; then
+        cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+        sed -i -e '/^TrustedUserCAKeys/d' -e '/^AuthorizedPrincipalsCommand/d' /etc/ssh/sshd_config
+        echo "AuthorizedPrincipalsCommand /usr/bin/adquery user -P %u" >> /etc/ssh/sshd_config
+        echo "AuthorizedPrincipalsCommandUser root" >> /etc/ssh/sshd_config
+        echo "TrustedUserCAKeys /etc/ssh/cps_ca.pub" >> /etc/ssh/sshd_config
+    fi
+    /usr/share/centrifydc/bin/curl -o /etc/ssh/cps_ca.pub https://$URL/servermanage/getmastersshkey    
 fi
 
 (
@@ -100,12 +127,13 @@ fi
     /usr/sbin/adleave -r && sleep 3 || true
 
     # join system to AD
-    # note that systemd does not start yet so adjoin will show error because
-    # the service cannot be started, this error (15) can be ignored
+    #
+    # the adjoin option -I requires Release 18.11 or later.
+    #
     echo "joining the system to AD ..."
     RC=0
-    /usr/sbin/adjoin -V $DOMAIN -z $ZONE "${CMDPARAM[@]}" --force || RC=$?
-    if [ $RC -eq 0 -o $RC -eq 15 ]; then
+    /usr/sbin/adjoin -V -I $DOMAIN -z $ZONE "${CMDPARAM[@]}" --force || RC=$?
+    if [ $RC -eq 0 ]; then
         # enable the service so that it will be started by systemd later on
         systemctl enable centrifydc.service
     else
@@ -113,17 +141,90 @@ fi
         false
     fi
 
-#   change password if specified
-    if [ "$ROOT_PASSWORD" != "" ] ; then
-       echo "set root password "
-       echo "$ROOT_PASSWORD" | passwd --stdin root
-    else
-       echo "generate root password "
-       /usr/share/centrifydc/bin/openssl rand -base64 16 | passwd --stdin root
+) 2>&1 | tee -a /var/centrify/adjoin.log
+
+if [ -x /usr/sbin/dad ]; then
+    (
+        echo "`date`: enable auditing service"
+        set -e
+
+        # enable auditing service, it should be running at anytime
+        systemctl enable centrifyda.service || true
+
+        # start auditing service for configuration
+        /usr/share/centrifydc/bin/centrifyda start || true
+
+        # enable or disable NSS auditing
+        if [[ "$NSSAuditing" == "Y" || "$NSSAuditing" == "y" || "$NSSAuditing" == "T" || "$NSSAuditing" == "t" ]] ; then
+            echo "enable NSS auditing "
+            /usr/sbin/dacontrol -e || true
+        else
+            echo "disable NSS auditing "
+            /usr/sbin/dacontrol -d || true
+        fi
+
+        # set audit installation
+        if [ -n "$INSTALLATION" ]; then
+
+            echo "set audit installation to $INSTALLATION"
+            /usr/sbin/dacontrol -i "$INSTALLATION" || true            
+
+            # the new installation setting will take effect once
+            # the auditing service is started by systemd later.
+        fi
+
+        # check current settings
+        /usr/sbin/dacontrol -q || true
+
+    ) 2>&1 | tee -a /var/centrify/adjoin.log
+fi
+
+# stop now, the service will be started by systemd later
+(
+    echo "stop Centrify DirectControl"
+    /usr/share/centrifydc/bin/centrifydc stop
+
+    if [ -x /usr/sbin/dad ]; then
+        echo "stop Centrify DirectAudit"
+        /usr/share/centrifydc/bin/centrifyda stop
     fi
 
 ) 2>&1 | tee -a /var/centrify/adjoin.log
 
+# change password if specified
+if [ "$ROOT_PASSWORD" != "" ] ; then
+    echo "set root password "
+    echo "$ROOT_PASSWORD" | passwd --stdin root
+else
+    echo "generate root password "
+    /usr/share/centrifydc/bin/openssl rand -base64 16 | passwd --stdin root
+fi
+
+# enroll cagent to add the docker to show as a system in cloud CS-46661
+if [[ "$UseMyAccount" == "Y" || "$UseMyAccount" == "y" || "$UseMyAccount" == "T" || "$UseMyAccount" == "t" ]] ; then
+    (
+    # create a new service for unenroll Centrify agent
+    systemctl enable centrifycc-unenroll.service
+
+    # note that the selinux policy set to enforcing
+    # change to permissive for now
+    sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+
+    if [ -f /etc/systemd/system/multi-user.target.wants/centrifycc.service ]; then
+        rm /etc/systemd/system/multi-user.target.wants/centrifycc.service
+    fi
+    touch /etc/centrifycc/*
+    touch /etc/centrifycc/autoedit/*
+    /usr/sbin/cunenroll -f
+    ) 2>&1 | tee -a /var/centrify/adjoin.log
+    # Connectors is optional
+    CENROLLPARAM=()
+    if [ "$CONNECTOR" != "" ] ; then
+        CENROLLPARAM=("-S" "\"Connectors:$CONNECTOR\"")
+    fi
+    # cenroll needs systemd service hence it is to be run at the end and in background process
+    /usr/sbin/cenroll -t $URL -F aapm --code $CODE --name $NAME --address $ADDRESS -S "Port:$PORT" -S "CertAuthEnable:true" "${CENROLLPARAM[@]}" -f &
+fi
 
 echo "start the container"
 exec /usr/sbin/init
